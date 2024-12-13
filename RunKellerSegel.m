a49c31bea2fac45dfc488d6c089ff71461458cc3
AndrewLKrause <42071282+AndrewLKrause@users.noreply.github.com>
2024-12-13 16:07:28 +0000

    Tweaked things

---
+++ RunKellerSegel.m
@@ -0,0 +1,19 @@
+%Delete everything in workspace
+clear;
+
+%Parameters you want to modify (put these beklow)
+%a = 
+%p = 
+%d = 
+%L = 
+%epsilon = ...
+
+%Parameters you don't care about (call another script)/setup
+%m, tspan, etc JUST IMPORTANT FROM ANOTHER SCRIPT
+%sets up Laplacians and other tedious nonsense
+
+%run ode15s or whatever (1 line)
+
+%code that computes amplitudes
+
+%plotting functions (1 plot per simulation)
